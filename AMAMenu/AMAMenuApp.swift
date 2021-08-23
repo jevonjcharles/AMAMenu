d342e887c806d84a9256a2768ef055d040a5b64c
Jevon Charles <jevoncharles@me.com>
2021-08-23 16:53:29 -0400

    Initial Commit

---
+++ AMAMenu/AMAMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  AMAMenuApp.swift
+//  AMAMenu
+//
+//  Created by Jevon Charles on 8/23/21.
+//
+
+import SwiftUI
+
+@main
+struct AMAMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AMAMenu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AMAMenu
+//
+//  Created by Jevon Charles on 8/23/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
